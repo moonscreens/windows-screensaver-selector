1ea8f55477770b5fcff6d223e84f755c42eda788
Caleb <CalebBabin@users.noreply.github.com>
2020-03-16 17:39:35 -0500

    add a proper blank screen

--- src/main.js
+++ src/main.js
@@ -8,9 +8,12 @@ window.addEventListener('DOMContentLoaded', ()=>{
 	})
 
 	const iframe = document.body.querySelector('.screensaver');
+	const tinyframe = document.body.querySelector('.monitor > iframe');
 	iframe.src = dropdown.selection;
+	tinyframe.src = iframe.src;
 	dropdown.onChange = (url) => {
 		iframe.src = (url) ? url : 'https://opl.io';
+		tinyframe.src = (url) ? url : 'https://opl.io';
 		if (url === undefined) {
 			iframe.setAttribute('style', 'display: none;');
 		} else {
