ef9b7987eb7b8d06eca075f586a230237ffe01a2
Caleb <CalebBabin@users.noreply.github.com>
2020-04-14 16:22:19 -0500

    add a cursor for OBS

--- src/App.js
+++ src/App.js
@@ -4,27 +4,32 @@ import './App.css';
 
 import Tabs from './components/Tabs';
 import ScreenSaverSelect from './pages/ScreenSaverSelect';
+import Cursor from './components/Cursor';
 
-
-function App() {
-	return (
-		<Window title="Display Properties" controlHelp={true}>
-			<Tabs activeTab={1}>
-				<div tab="Background">
-					First tabs content
-				</div>
-				<div tab="Screen Saver">
-					<ScreenSaverSelect />
-				</div>
-				<div tab="Appearance">
-					Third tabs content
-				</div>
-				<div tab="Settings">
-					Fourth tabs content
-				</div>
-			</Tabs>
-		</Window>
-	);
+class App extends React.Component {
+	render () {
+		return (
+			<>
+				<Cursor />
+				<Window title="Display Properties" controlHelp={true}>
+					<Tabs activeTab={1}>
+						<div tab="Background">
+							First tabs content
+						</div>
+						<div tab="Screen Saver">
+							<ScreenSaverSelect />
+						</div>
+						<div tab="Appearance">
+							Third tabs content
+						</div>
+						<div tab="Settings">
+							Fourth tabs content
+						</div>
+					</Tabs>
+				</Window>
+			</>
+		);
+	}
 }
 
 export default App;
---
+++ src/components/Cursor.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const CursorElement = styled.div`
+	background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAsAAAATCAYAAABGKffQAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAB+SURBVDhPjZIJDsAgCATRp9V326/RLA0GFcRJejq7sdpCRNzfjmtKxak9jeUpQWRmvgqIDG4CQwZZYJLBKbDJIAq4MvACoQzWwFEGNiA7iBcrpeybOskQ7P36G4xp6KA2IuR+oNfiIbIVT+31plFxly5qT9fZEsrajuOfKtEHjKJYo0XefREAAAAASUVORK5CYII=');
+	background-repeat: no-repeat;
+	background-position: top left;
+	position: fixed;
+	width: 11px;
+	height: 19px;
+	pointer-events: none;
+	z-index: 999999;
+`;
+
+class Cursor extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			display: 'none',
+			left: 0,
+			top: 0,
+			lastInput: 0,
+		}
+		this.mouseListenerBound = this.mouseListener.bind(this);
+	}
+	mouseListener (e) {
+		this.setState({
+			left: e.clientX,
+			top: e.clientY,
+			display: 'block',
+			lastInput: Date.now(),
+		});
+	}
+	componentDidMount () {
+		window.addEventListener('mousemove', this.mouseListenerBound);
+	}
+	componentWillUnmount () {
+		window.removeEventListener('mousemove', this.mouseListenerBound);
+	}
+	render () {
+		return <CursorElement style={{
+			display: this.state.display,
+			left: this.state.left,
+			top: this.state.top
+		}} />
+	}
+}
+
+export default Cursor;
